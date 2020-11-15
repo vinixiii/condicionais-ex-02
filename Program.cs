14751d7c9d9f175b5a76bb134d3fbad57ffae229
Vinícius <vinissfigueiroa@gmail.com>
2020-11-15 18:28:05 -0300

    Exercício 02 - Feito

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercise_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício - 02");
+            Console.WriteLine("----------");
+            Console.WriteLine("");
+
+            int anoAtual = 2020;
+
+            Console.WriteLine("Digite o ano de seu nascimento:");
+            int anoDeNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            int idadeEmAnos = anoAtual - anoDeNascimento;
+            int idadeEmSemanas = idadeEmAnos * 52;
+
+            Console.WriteLine("Você tem " + idadeEmAnos + " anos. Isso é equivalente há " + idadeEmSemanas + " semanas.");
+        }
+    }
+}
